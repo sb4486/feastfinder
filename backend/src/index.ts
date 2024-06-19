50df86c96d4698f110c76ee12d6490c750c73f36
lsb4486 <lsb4486@drexel.edu>
2024-06-19 10:46:48 -0400

    Latest

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,18 +2,21 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
 
 mongoose
     .connect(process.env.MONGODB_CONNECTION_STRING as string)
     .then(() => console.log("Connected to database!"));
 
 const app = express();
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get("/test", async (req: Request,res: Response)=>{
-    res.json({ message: "Hello!" });
+app.get("/health", async (req: Request, res: Response)=>{
+    res.send({ message: "health OK!" });
 });
+
+app.use("/api/my/user", myUserRoute)
 
 app.listen(7001, ()=> {
     console.log("server started on localhost:7001");
